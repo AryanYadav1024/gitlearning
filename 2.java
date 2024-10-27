989a1f14896e9223919c5e6e1a2b232eb27780c5
Aryan yadav <aryan0y7@gmail.com>
2024-10-27 19:37:04 +0530

    adding to mergingtest branch to merge with main

---
+++ 2.java
@@ -0,0 +1,14 @@
+public class a0_basic {// the first class is going to be a public class also remember to name the class with first capital letter
+    // public is access modifier it means it would be accessed from anywhere.
+    //class is just a group of properties and functions
+    //a0_basic is just the name of the file
+    public static void main(String[] args) {
+        // main function where the program starts the main word is reserved, it is the entry point of java program
+        // it is public as the class is public
+        // static - to run this main function without creating the object of class demo, so that it is a class level member and belongs to all instances of the class
+        //void - return type of the function as we don't need it to give any type of value
+        //String[] args is command line arguments collection of string whatever arguments we are giving in the terminal it will print
+        System.out.println("hello worldq");
+        
+    }
+}
